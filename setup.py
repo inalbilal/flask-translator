b914e4e3738e2d3f2c1e431ac54d13236340ef97
INAL <01bilalinal@gmail.com>
2023-12-23 16:59:56 +0300

    Changed Setup

--- setup.py
+++ setup.py
@@ -4,25 +4,43 @@ Flask-Translator
 
 Flask-Translator is a quick and easy-to-use extension for Flask projects, enabling translation from YAML files.
 """
-from setuptools import setup
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.1'
+DESCRIPTION = 'Flask-Translator is a quick and easy-to-use extension for Flask projects, enabling translation from YAML files.'
 
 setup(
     name='Flask-Translator',
-    version='1.0',
+    version=VERSION,
     url='https://github.com/inalbilal/flask-translator/',
     license='BSD',
     author='Bilal Inal',
     author_email='01bilalinal@gmail.com',
-    description='Flask-Translator is a quick and easy-to-use extension for Flask projects, enabling translation from '
-                'YAML files.',
-    long_description=__doc__,
-    py_modules=['flask_translator'],
-    zip_safe=False,
-    include_package_data=True,
-    platforms='any',
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
     install_requires=[
         'Flask',
         'PyYAML'
+    ],
+    keywords=[
+        'Flask',
+        'Flask extension',
+        'translation',
+        'i18n',
+        'l10n',
+        'multilingual',
+        'language',
+        'internationalization',
+        'localization',
     ],
     classifiers=[
         'Environment :: Web Environment',
